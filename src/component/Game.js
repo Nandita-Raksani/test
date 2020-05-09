72f0d6391d02731d25b524d2f666e1c9eb93a340
1132008 <nrak15@gmail.com>
2020-05-09 17:40:07 +0530

    refactor: Extracted the hardcoded values into constants

--- src/component/Game.js
+++ src/component/Game.js
@@ -6,12 +6,12 @@ import Constants from '../constants/Constants';
 
 const Game = () => {
     const [state, setState] = useState({
-        boardArray: Array(Constants.NUMBER_OF_TILES).fill('')
+        boardArray: Array(Constants.NUMBER_OF_TILES).fill(Constants.EMPTY_VALUE)
     });
 
     const renderBoard = () => {
         let tileList = [];
-        for (let position = 0; position < Constants.NUMBER_OF_TILES; position++) {
+        for (let position = Constants.INITIAL_TILE_VALUE; position < Constants.NUMBER_OF_TILES; position++) {
             tileList.push(<li key={position}>
                 <Tile onClick={() => handleTileClick(position)} value={state.boardArray[position]} />
             </li>);
@@ -21,7 +21,7 @@ const Game = () => {
 
     const handleTileClick = (position) => {
         const boardArray = state.boardArray.slice();
-        boardArray[position] = 'X';
+        boardArray[position] = Constants.SYMBOL_X;
         setState((prevState) => ({ ...prevState, boardArray: boardArray }));
     }
 
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,5 +1,8 @@
 const Constants = {
     APP_TITLE: "Tic-Tac-Toe",
-    NUMBER_OF_TILES: 9
+    NUMBER_OF_TILES: 9,
+    EMPTY_VALUE: "",
+    SYMBOL_X: "X",
+    INITIAL_TILE_VALUE: 0
 };
 export default Constants;
