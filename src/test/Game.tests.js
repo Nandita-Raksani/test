6ae2552c6211b2e5ff06b6c9cd5d850b871718b3
1132008 <nrak15@gmail.com>
2020-05-09 17:35:38 +0530

    test: Extracted repeated lines into reusable method

--- src/test/Game.tests.js
+++ src/test/Game.tests.js
@@ -20,8 +20,13 @@ describe(("<Game/> component"), () => {
 });
 
 describe(("<Game/> component functionality"), () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = mount(<Game />);
+    });
+
     it("Should render 9 empty Tiles", () => {
-        let wrapper = mount(<Game />);
         expect(wrapper.find(Tile).length).toBe(9);
         wrapper.find(Tile).forEach(square => {
             expect(square.find('button').text()).toBe('');
@@ -29,7 +34,6 @@ describe(("<Game/> component functionality"), () => {
     })
 
     it("Should always assign first move to Player X", () => {
-        let wrapper = mount(<Game />);
         wrapper.find(Tile).at(0).find('button').simulate('click');
         expect(wrapper.find(Tile).at(0).find('button').text()).toBe('X');
     })
